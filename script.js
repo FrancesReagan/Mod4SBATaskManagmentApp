dfa250716d5252ac72b0346878dd67372a19ddd4
Frances Reagan James <adtgwen@gmail.com>
2025-05-19 15:34:39 -0400

    added dom elements to get in javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+//get DOM elements//
+const taskNameInput = document.getElementById("taskName");
+const taskCategorySelect = document.getElementById("taskCategory");
+const taskDeadlineInput = document.getElementById("taskDeadline");
+const taskStatusSelect = document.getElementById("taskStatus");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskListBody = document.getElementById("taskListBody");
+const statusFilter = document.getElementById("statusFilter");
+const catergoryFilter = document.getElementById("categoryFilter");
+const noTasksMessage = document.getElementById("noTasksMessage");
+
+
